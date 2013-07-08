e5a0569053b3bddfa361b3910a12276f05843a8c
Elliot Smith <elliot.smith@intel.com>
2013-07-08 13:25:26 +0100

    Refactor so sequence of operations is clearer and code cleaner
    *   Split data gathering from string creation. Make sure we have
        all the data required by the template before we start
        trying to create the output zip file name.
    
    *   Move the code for packing the zip file (packFiles()) and the
        code for getting the git commit (gitCommitId()) out of the main
        task definition and into private functions. This makes it easier
        to follow the code.
    
    *   Use a template string for the zip file name. This is cleaner
        (it's clear how the data will be interpolated into the template
        string for the zip filename, as it's just a lodash template).
    
    *   Files can be added to zip file in parallel.
    
        Can use async.each() rather than async.forEachSeries(), as the
    order in which files are added to the output zipfile doesn't
    matter.
    
        This might make creation of the zip file slightly faster (and
    if it doesn't it doesn't matter anyway).

--- tasks/grunt-zipup.js
+++ tasks/grunt-zipup.js
@@ -6,6 +6,7 @@
  * http://www.apache.org/licenses/LICENSE-2.0
  *
  */
+
 module.exports = function (grunt) {
   'use strict';
 
@@ -14,23 +15,98 @@ module.exports = function (grunt) {
   var async = require('async');
   var AdmZip = require('adm-zip');
 
+  // get the latest commit ID as an 8-character string;
+  // if not a git repo, returns '' and logs an error;
+  // receiver is a function with the signature receiver(err, result),
+  // where err is null if no error occurred and result is the commit ID
+  var gitCommitId = function (cb) {
+    grunt.util.spawn(
+      {
+        cmd: 'git',
+        args: ['log', '-n1', '--format=format:\'%h\'']
+      },
+
+      function (err, result) {
+        if (err) {
+          cb(err);
+        }
+        else {
+          cb(null, result.stdout.replace(/'/g, ''));
+        }
+      }
+    );
+  };
+
+  // returns true if path exists and is not a directory
+  var isFile = function (path) {
+    if (!fs.existsSync(path) || fs.statSync(path).isDirectory()) {
+      return false;
+    }
+
+    return true;
+  };
+
+  // infiles is a grunt files object: an array with objects
+  // which map from src to dest
+  var packFiles = function (outDir, template, data, files, cb) {
+    var zipfilename = grunt.template.process(template, {data: data});
+    var outfile = path.join(outDir, zipfilename);
+    var zipfile = new AdmZip();
+
+    async.each(
+      files,
+      function (file, next) {
+        var src = file.src[0];
+        var dest = file.dest || src;
+
+        if (isFile(src)) {
+          grunt.log.writeln('adding ' + src + ' to package as ' + dest);
+
+          var buffer = fs.readFileSync(src);
+
+          zipfile.addFile(dest, buffer);
+
+          next();
+        }
+        else {
+          next();
+        }
+      },
+      function (err) {
+        if (err) {
+          grunt.fatal(err.message);
+        }
+        else {
+          grunt.log.writeln('\npackage written to:\n' + outfile);
+          zipfile.writeZip(outfile);
+          cb();
+        }
+      }
+    );
+  };
+
   // zipup task definition; see the README.md file for options
   grunt.registerMultiTask(
     'zipup',
     'Zip files with custom zipfile name',
     function (identifier) {
+      var done = this.async();
+      identifier = (identifier ? '_' + identifier : '');
+
+      // default template for the filename
+      var defaultTemplate = '<%= appName %>_<%= version %>_' +
+                            '<% if (gitCommit) { %>' +
+                            'git@<%= gitCommit %>_' +
+                            '<% } %>' +
+                            '<%= datetime %><%= identifier %>.<%= suffix %>';
+
+      // pick data out of the task options and set defaults if not present
       var appName = this.data.appName;
       var version = this.data.version;
 
-      if (!appName) {
-        grunt.fatal('zipup task requires appName argument');
-      }
-
-      if (!version) {
-        grunt.fatal('zipup task requires version argument (x.x.x)');
-      }
-
       var suffix = this.data.suffix || 'zip';
+      suffix = suffix.replace(/^\./, '');
+
       var outDir = this.data.outDir || '.';
       var addGitCommitId = !!this.data.addGitCommitId;
 
@@ -48,105 +124,52 @@ module.exports = function (grunt) {
         grunt.file.mkdir(outDir);
       }
 
-      var done = this.async();
+      // use custom template if defined; NB if this is the case, it's up to
+      // the user to ensure that all the required data is present in
+      // the zipup task configuration
+      var template = this.data.template;
 
-      var outFile = appName;
-
-      // get the latest commit ID as an 8-character string;
-      // if not a git repo, returns '' and logs an error;
-      // receiver is a function with the signature receiver(err, result),
-      // where err is null if no error occurred and result is the commit ID
-      var gitCommitId = function (receiver) {
-        grunt.util.spawn(
-          {
-            cmd: 'git',
-            args: ['log', '-n1', '--format=format:\'%h\'']
-          },
-
-          function (err, result) {
-            if (err) {
-              receiver(true, '');
-            }
-            else {
-              receiver(null, result.stdout.replace(/'/g, ''));
-            }
-          }
-        );
-      };
-
-      var isFile = function (path) {
-        if (!fs.existsSync(path) || fs.statSync(path).isDirectory()) {
-          return false;
+      // if no custom template set, use the default one
+      if (!template) {
+        // make sure the default template has the data it needs
+        if (!appName) {
+          grunt.fatal('zipup task requires appName argument');
         }
 
-        return true;
-      };
-
-      // infiles is a grunt files object: an array with objects
-      // which map from src to dest
-      var packFiles = function (outfile, infiles, cb) {
-        var zipfile = new AdmZip();
-        var buffer;
-
-        async.forEachSeries(
-          infiles,
-          function (file, next) {
-            var src = file.src[0];
-            var dest = file.dest || src;
-
-            if (isFile(src)) {
-              grunt.log.writeln('adding ' + src + ' to package as ' + dest);
-
-              buffer = fs.readFileSync(src);
-
-              zipfile.addFile(dest, buffer);
-
-              next();
-            }
-            else {
-              next();
-            }
-          },
-          function (err) {
-            if (err) {
-              grunt.fatal(err.message);
-            }
-            else {
-              grunt.log.writeln('\npackage written to:\n' + outfile);
-              zipfile.writeZip(outfile);
-              cb();
-            }
-          }
-        );
-      };
-
-      var receiver = function (err, result) {
-        if (version) {
-          outFile += '_' + version;
+        if (!version) {
+          grunt.fatal('zipup task requires version argument (x.x.x)');
         }
 
-        if (result) {
-          outFile += '_git@' + result;
-        }
+        // data is OK, so set template to the default
+        template = defaultTemplate;
+      }
 
-        outFile += '_' + grunt.template.today('yyyy-mm-dd_HHMMss');
-
-        if (identifier) {
-          outFile += '_' + identifier;
-        }
-
-        outFile += '.' + suffix.replace(/^\./, '');
-        outFile = path.join(outDir, outFile);
-
-        packFiles(outFile, files, done);
+      // data to put into the template
+      var data = {
+        appName: appName,
+        version: version,
+        suffix: suffix,
+        identifier: identifier,
+        datetime: grunt.template.today('yyyy-mm-dd_HHMMss')
       };
 
-      // append git commit ID to the package name
+      // do we want to use the git commit ID in the template?
       if (addGitCommitId) {
-        gitCommitId(receiver);
+        // add the git commit data before generating filename
+        gitCommitId(function (err, commit) {
+          if (err) {
+            done(err);
+          }
+          else {
+            data.gitCommit = commit;
+            packFiles(outDir, template, data, files, done);
+          }
+        });
       }
       else {
-        receiver(false, null);
+        // set an empty git commit ID
+        data.gitCommit = null;
+        packFiles(outDir, template, data, files, done);
       }
     }
   );
