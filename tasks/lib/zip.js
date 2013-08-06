ad13e819ce5dee1699273ef2e9e9205133cdc292
Elliot Smith <elliot.smith@intel.com>
2013-08-06 10:25:31 +0100

    Use moxie-zip instead of node-archiver
    After switching to node-archiver, I noticed some issues with
    zip file creation, where the zip would occasionally be corrupt.
    This seemed to be more of a problem with other projects, where
    only a single zip file was being generated at a time (unlike
    the test environment for this project, where multiple zip
    files are created in series).
    
    I tried zipstream, which also suffered the same issues.
    
    However, moxie-zip, while quite a bit slower, does actually
    consistently write working zip files. I switched the code over
    to use it instead of node-archiver.

--- tasks/lib/zip.js
+++ tasks/lib/zip.js
@@ -1,27 +1,23 @@
 // wrapper for whichever zip implementation I'm using at the moment
-var fs = require('fs');
-var archiver = require('archiver');
+var ZipWriter = require('moxie-zip').ZipWriter;
 
 (function () {
   'use strict';
 
   var Zip = function (outfile) {
-    this.wrapped = archiver('zip');
-    var out = fs.createWriteStream(outfile);
-    this.wrapped.pipe(out);
-
-    this.wrapped.on('error', function (err) {
-      throw err;
-    });
+    this.wrapped = new ZipWriter();
+    this.outfile = outfile;
   };
 
   Zip.prototype.addFile = function (src, dest, cb) {
-    var buf = fs.readFileSync(src);
-    this.wrapped.append(buf, { name: dest }, cb);
+    this.wrapped.addFile(dest, src);
+    cb();
   };
 
   Zip.prototype.writeToFile = function (cb) {
-    this.wrapped.finalize(cb);
+    this.wrapped.saveAs(this.outfile, function () {
+      cb();
+    });
   };
 
   module.exports = {
